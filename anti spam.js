4b1fdd94dbdaede1ffb866e8a2bfc30c2907c72f
Revanvht <48178383+Revanvht@users.noreply.github.com>
2019-03-03 12:43:24 +0000

    Create anti spam.js

---
+++ anti spam.js
@@ -0,0 +1,43 @@
+const Discord = require("discord.js");
+const client = new Discord.Client()
+const antispam = require("discord-anti-spam");
+
+
+
+client.on('ready', () => {
+
+
+	antispam(client, {
+
+		warnBuffer: 3,
+
+		maxBuffer: 5,
+
+		interval: 1000,
+
+		warningMessage: "Jangan Spam Oiii,,Sanksi Banned PERMANENT @here",
+
+		maxDuplicatesWarning: 4,
+
+		maxDuplicatesBan: 5,
+
+		deleteMessageAfterBanForPastDay: 7,
+
+		exemptRoles: [],
+
+		exemptUser: [],
+
+});
+
+
+});
+
+
+
+client.on('message', msg => {
+
+
+  client.emit('checkMessage', msg);
+
+})
+client.login(process.env.BOT);
